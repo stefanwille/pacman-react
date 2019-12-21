7758791fc14902a96ee2925a6489c5e494d3cac4
Stefan Wille <github@stefanwille.com>
2019-12-21 14:01:02 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React, { FC, useEffect, useState, useMemo } from "react";
+import React, { FC, useEffect, useState, Fragment } from "react";
 import "./App.css";
 
 type Direction = "UP" | "DOWN" | "RIGHT" | "LEFT";
 type PacManPhase = 0 | 1;
+type GhostNumber = 0 | 1 | 2 | 3;
 
 const DY = {
   LEFT: "0px",
@@ -24,7 +25,7 @@ const PacMan: FC<PacManProps> = ({ direction, phase, x, y }) => {
   const dy: string = DY[direction];
   return (
     <div
-      className="PacMan"
+      className="Sprite"
       style={{
         backgroundPosition: `${dx} ${dy}`,
         transform: `translate(${x}px, ${y}px) scale(2)`
@@ -32,6 +33,37 @@ const PacMan: FC<PacManProps> = ({ direction, phase, x, y }) => {
     />
   );
 };
+
+type GhostProps = {
+  direction: Direction;
+  phase: PacManPhase;
+  x: number;
+  y: number;
+  ghostNumber: number;
+};
+
+const GHOST_DX = {
+  RIGHT: 0,
+  LEFT: 32,
+  UP: 64,
+  DOWN: 48
+};
+
+const Ghost: FC<GhostProps> = ({ direction, phase, x, y, ghostNumber }) => {
+  const dx: string = `-${457 + GHOST_DX[direction] + phase * 16}px`;
+  const dy = `-${65 + ghostNumber * 16}px`;
+  return (
+    <div
+      className="Sprite"
+      style={{
+        backgroundPosition: `${dx} ${dy}`,
+        transform: `translate(${x}px, ${y}px) scale(2)`
+      }}
+    />
+  );
+};
+
+const GhostNumbers: GhostNumber[] = [0, 1, 2, 3];
 
 const App: React.FC = () => {
   const [phase, setPhase] = useState<PacManPhase>(0);
@@ -58,6 +90,67 @@ const App: React.FC = () => {
         <PacMan direction="DOWN" phase={1} x={100} y={380} />
         <PacMan direction="RIGHT" phase={0} x={100} y={460} />
         <PacMan direction="RIGHT" phase={1} x={100} y={500} />
+
+        {GhostNumbers.map((ghostNumber: GhostNumber) => (
+          <Fragment key={ghostNumber}>
+            <Ghost
+              direction="LEFT"
+              phase={0}
+              x={160 + ghostNumber * 40}
+              y={100}
+              ghostNumber={ghostNumber}
+            />
+            <Ghost
+              direction="LEFT"
+              phase={1}
+              x={160 + ghostNumber * 40}
+              y={140}
+              ghostNumber={ghostNumber}
+            />
+            <Ghost
+              direction="UP"
+              phase={0}
+              x={160 + ghostNumber * 40}
+              y={220}
+              ghostNumber={ghostNumber}
+            />
+            <Ghost
+              direction="UP"
+              phase={1}
+              x={160 + ghostNumber * 40}
+              y={260}
+              ghostNumber={ghostNumber}
+            />
+            <Ghost
+              direction="DOWN"
+              phase={0}
+              x={160 + ghostNumber * 40}
+              y={340}
+              ghostNumber={ghostNumber}
+            />
+            <Ghost
+              direction="DOWN"
+              phase={1}
+              x={160 + ghostNumber * 40}
+              y={380}
+              ghostNumber={ghostNumber}
+            />
+            <Ghost
+              direction="RIGHT"
+              phase={0}
+              x={160 + ghostNumber * 40}
+              y={460}
+              ghostNumber={ghostNumber}
+            />
+            <Ghost
+              direction="RIGHT"
+              phase={1}
+              x={160 + ghostNumber * 40}
+              y={500}
+              ghostNumber={ghostNumber}
+            />
+          </Fragment>
+        ))}
       </div>
     </div>
   );
