d84096ebd59cdd0e9cc3c624f157157d3634ee91
Stefan Wille <github@stefanwille.com>
2019-12-21 22:59:28 +0100

    .

--- src/pages/AnimationTestPage/AnimationTestPage.tsx
+++ src/pages/AnimationTestPage/AnimationTestPage.tsx
@@ -1,38 +1,51 @@
 import React, { useEffect } from "react";
 import { Ghost, GhostPhase } from "../../components/Ghost";
-import { useLocalStore, observer } from "mobx-react-lite";
+import { observer } from "mobx-react-lite";
+import { observable, action, computed } from "mobx";
 import { Sprite } from "../../components/Sprite";
 
 const SPEED = 1.5;
 
-const useMyLocalStore = () => {
-  const store = useLocalStore(() => ({
-    x: 16,
-    vx: SPEED,
-    phase: 0,
-    gameRunning: true,
+class GhostStore {
+  @observable
+  timestamp = 0;
 
-    update(timestamp: number) {
-      store.x += store.vx;
-      if (store.x > 17 * 16) {
-        store.vx = -1 * SPEED;
-      }
-      if (store.x <= 16) {
-        store.vx = 1 * SPEED;
-      }
-      store.phase = Math.round(timestamp / 300) % 2;
-    },
+  @observable
+  x = 16;
 
-    stopGame() {
-      store.gameRunning = false;
+  @observable
+  vx = SPEED;
+
+  @computed
+  get phase(): number {
+    return Math.round(this.timestamp / 300) % 2;
+  }
+
+  @observable
+  gameRunning = true;
+
+  @action.bound
+  update(timestamp: number) {
+    this.timestamp = timestamp;
+
+    this.x += this.vx;
+    if (this.x > 17 * 16) {
+      this.vx = -1 * SPEED;
     }
-  }));
-  return store;
-};
+    if (this.x <= 16) {
+      this.vx = 1 * SPEED;
+    }
+  }
+
+  @action.bound
+  stopGame() {
+    this.gameRunning = false;
+  }
+}
+
+const store = new GhostStore();
 
 export const AnimationTestPage: React.FC = observer(() => {
-  const store = useMyLocalStore();
-
   const animationStep = (timestamp: number) => {
     store.update(timestamp);
     if (store.gameRunning) {
