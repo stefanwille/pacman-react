4d265f12ed68f805bcf13101d815bc407ea321a0
Stefan Wille <github@stefanwille.com>
2019-12-21 13:47:56 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React, { FC } from "react";
+import React, { FC, useEffect, useState, useMemo } from "react";
 import "./App.css";
 
 type Direction = "UP" | "DOWN" | "RIGHT" | "LEFT";
-type PacManPhase = 0 | 1 | 2;
+type PacManPhase = 0 | 1;
 
 const DY = {
   LEFT: "0px",
@@ -12,13 +12,15 @@ const DY = {
   DOWN: "-48px"
 };
 
-const PacMan: FC<{
+type PacManProps = {
   direction: Direction;
   phase: PacManPhase;
   x: number;
   y: number;
-}> = ({ direction, phase, x, y }) => {
-  const dx = `-${phase * 16}px`;
+};
+
+const PacMan: FC<PacManProps> = ({ direction, phase, x, y }) => {
+  const dx = `-${456 + phase * 16}px`;
   const dy: string = DY[direction];
   return (
     <div
@@ -32,21 +34,30 @@ const PacMan: FC<{
 };
 
 const App: React.FC = () => {
+  const [phase, setPhase] = useState<PacManPhase>(0);
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setPhase((phase: PacManPhase) => (phase === 0 ? 1 : 0));
+    }, 300);
+    return () => {
+      clearInterval(timerId);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className="App">
       <div className="Maze">
-        <PacMan direction="LEFT" phase={0} x={0} y={0} />
+        <PacMan direction="LEFT" phase={phase} x={30} y={100} />
+
+        <PacMan direction="LEFT" phase={0} x={100} y={100} />
         <PacMan direction="LEFT" phase={1} x={100} y={140} />
-        <PacMan direction="LEFT" phase={2} x={100} y={180} />
         <PacMan direction="UP" phase={0} x={100} y={220} />
         <PacMan direction="UP" phase={1} x={100} y={260} />
-        <PacMan direction="UP" phase={2} x={100} y={300} />
         <PacMan direction="DOWN" phase={0} x={100} y={340} />
         <PacMan direction="DOWN" phase={1} x={100} y={380} />
-        <PacMan direction="DOWN" phase={2} x={100} y={420} />
         <PacMan direction="RIGHT" phase={0} x={100} y={460} />
         <PacMan direction="RIGHT" phase={1} x={100} y={500} />
-        <PacMan direction="RIGHT" phase={2} x={100} y={540} />
       </div>
     </div>
   );
