9a05f1932aa402ae3d90f1590aa74f5f2801273a
Stefan Wille <github@stefanwille.com>
2019-12-22 13:50:45 +0100

    .

--- src/pages/MazeTestPage/MazeTestPage.tsx
+++ src/pages/MazeTestPage/MazeTestPage.tsx
@@ -4,14 +4,13 @@ import { Sprite } from "../../components/Sprite";
 export const MazeTestPage: React.FC = () => {
   return (
     <div>
-      <Sprite className="Sprite-maze" name="maze-state-full" x={0} y={0} />;
+      <Sprite className="Sprite-maze" name="maze-state-empty" x={0} y={0} />
       <Sprite
         className="Sprite-maze"
-        name="maze-state-empty"
-        x={0}
-        y={250 * 4}
+        name="maze-state-full"
+        x={16 * 16 * 3}
+        y={0}
       />
-      ;
     </div>
   );
 };
