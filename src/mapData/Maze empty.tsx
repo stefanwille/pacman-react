b00c23ba87b9e83006671a7f625a34f4f49eea1f
Stefan Wille <github@stefanwille.com>
2019-12-24 15:02:07 +0100

    .

---
+++ src/mapData/Maze empty.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Maze empty" tilewidth="8" tileheight="8" tilecount="868" columns="28">
+ <image source="arcade empty.png" width="224" height="248"/>
+</tileset>
---
+++ src/mapData/Maze full.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Maze full" tilewidth="8" tileheight="8" tilecount="868" columns="28">
+ <image source="arcade full.png" width="224" height="248"/>
+</tileset>
