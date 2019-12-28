aabc0172045acc2f7f5935a469da1cdc702c771b
Stefan Wille <github@stefanwille.com>
2019-12-28 12:59:25 +0100

    .

--- src/pages/AnimationTestPage/AnimationTestPage.tsx
+++ src/pages/AnimationTestPage/AnimationTestPage.tsx
@@ -1,10 +1,36 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, FC } from "react";
 import { Ghost } from "../../components/Ghost";
 import { observer } from "mobx-react-lite";
 import { Sprite } from "../../components/Sprite";
 import { GameStore, GhostStore } from "../../lib/Store";
 import { PacMan } from "../../components/PacMac";
 import { onTimeElapsed } from "../../lib/onTimeElapsed";
+
+const PacManView: FC<{ store: GameStore }> = observer(({ store }) => {
+  return (
+    <PacMan
+      direction={store.pacMan.direction}
+      phase={store.pacMan.phase}
+      x={store.pacMan.x}
+      y={store.pacMan.y}
+    />
+  );
+});
+
+const GhostView: FC<{ store: GameStore; ghostNumber: number }> = observer(
+  ({ store, ghostNumber }) => {
+    const ghostStore: GhostStore = store.ghosts[ghostNumber];
+    return (
+      <Ghost
+        direction={ghostStore.direction}
+        phase={ghostStore.phase}
+        x={ghostStore.x}
+        y={ghostStore.y}
+        ghostNumber={ghostStore.ghostNumber}
+      />
+    );
+  }
+);
 
 export const AnimationTestPage: React.FC = observer(() => {
   const [store] = useState(() => new GameStore());
@@ -42,22 +68,10 @@ export const AnimationTestPage: React.FC = observer(() => {
     <div>
       <Sprite className="Sprite-maze" name="maze-state-full" x={0} y={10} />
 
-      <PacMan
-        direction={store.pacMan.direction}
-        phase={store.pacMan.phase}
-        x={store.pacMan.x}
-        y={store.pacMan.y}
-      />
+      <PacManView store={store} />
 
-      {store.ghosts.map((ghost: GhostStore, index: number) => (
-        <Ghost
-          key={index}
-          direction={ghost.direction}
-          phase={ghost.phase}
-          x={ghost.x}
-          y={ghost.y}
-          ghostNumber={ghost.ghostNumber}
-        />
+      {store.ghosts.map((_, index: number) => (
+        <GhostView store={store} ghostNumber={index} key={index} />
       ))}
     </div>
   );
