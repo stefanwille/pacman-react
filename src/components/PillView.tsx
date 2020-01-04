5a411561e08b914487a58b8a19d4a7e673680750
Stefan Wille <github@stefanwille.com>
2020-01-04 15:46:00 +0100

    .

---
+++ src/components/PillView.tsx
@@ -0,0 +1,56 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { FC, Fragment } from 'react';
+import { observer } from 'mobx-react-lite';
+import { useStore } from '../lib/StoreContext';
+import { Box } from '../components/Box';
+import { Sprite } from './Sprite';
+import { getPillHitBox } from '../lib/onTimeElapsed';
+import {
+  BASIC_PILL_ID,
+  MAZE_HEIGHT_IN_TILES,
+  MAZE_WIDTH_IN_TILES,
+  ENERGIZER_ID,
+} from '../lib/MazeData';
+import { screenFromTileCoordinate } from '../lib/Coordinates';
+
+const BasicPillView: FC<{ x: number; y: number }> = ({ x, y }) => (
+  <Sprite x={x - 10} y={y - 10} name="basic-pill" />
+);
+
+const EnergizerView: FC<{ x: number; y: number }> = ({ x, y }) => (
+  <Sprite x={x - 10} y={y - 10} name="energizer" />
+);
+
+export const BasicPillHitBox: FC<{}> = () => {
+  const rect = getPillHitBox(1, 3, BASIC_PILL_ID);
+  return <Box rect={rect} color="blue" />;
+};
+
+export const PillsView: FC<{}> = observer(() => {
+  const store = useStore();
+  const views = [];
+  for (let ty = 0; ty < MAZE_HEIGHT_IN_TILES; ty++) {
+    for (let tx = 0; tx < MAZE_WIDTH_IN_TILES; tx++) {
+      const tileId = store.pills[ty][tx];
+      if (tileId === BASIC_PILL_ID) {
+        views.push(
+          <BasicPillView
+            key={`${tx}/${ty}`}
+            x={screenFromTileCoordinate(tx)}
+            y={screenFromTileCoordinate(ty)}
+          />
+        );
+      }
+      if (tileId === ENERGIZER_ID) {
+        views.push(
+          <EnergizerView
+            key={`${tx}/${ty}`}
+            x={screenFromTileCoordinate(tx)}
+            y={screenFromTileCoordinate(ty)}
+          />
+        );
+      }
+    }
+  }
+  return <Fragment>{views}</Fragment>;
+});
--- src/pages/AnimationTestPage/AnimationTestPage.tsx
+++ src/pages/AnimationTestPage/AnimationTestPage.tsx
@@ -1,71 +1,19 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useEffect, useCallback, FC, Fragment } from 'react';
+import React, { useEffect, useCallback, FC } from 'react';
 import { GhostsView } from '../../components/Ghost';
 import { observer } from 'mobx-react-lite';
 import { Sprite } from '../../components/Sprite';
 import { PacManView } from '../../components/PacMac';
 import { useGameLoop } from '../../lib/useGameLoop';
-import {
-  screenFromTile,
-  screenFromTileCoordinate,
-} from '../../lib/Coordinates';
-import {
-  MAZE_HEIGHT_IN_TILES,
-  MAZE_WIDTH_IN_TILES,
-  BASIC_PILL_ID,
-  ENERGIZER_ID,
-} from '../../lib/MazeData';
-import { getPillHitBox } from '../../lib/onTimeElapsed';
+import { screenFromTile } from '../../lib/Coordinates';
 import { useStore, StoreContext } from '../../lib/StoreContext';
 import { action } from 'mobx';
-import { Box } from '../../components/Box';
 import { GameStore } from '../../lib/GameStore';
+import { PillsView } from '../../components/PillView';
 
 const MazeView: FC<{}> = () => (
   <Sprite className="Sprite-maze" name="maze-state-empty" x={0} y={0} />
 );
-
-const BasicPillView: FC<{ x: number; y: number }> = ({ x, y }) => (
-  <Sprite x={x - 10} y={y - 10} name="basic-pill" />
-);
-
-const EnergizerView: FC<{ x: number; y: number }> = ({ x, y }) => (
-  <Sprite x={x - 10} y={y - 10} name="energizer" />
-);
-
-export const BasicPillHitBox: FC<{}> = () => {
-  const rect = getPillHitBox(1, 3, BASIC_PILL_ID);
-  return <Box rect={rect} color="blue" />;
-};
-
-const PillsView: FC<{}> = observer(() => {
-  const store = useStore();
-  const views = [];
-  for (let ty = 0; ty < MAZE_HEIGHT_IN_TILES; ty++) {
-    for (let tx = 0; tx < MAZE_WIDTH_IN_TILES; tx++) {
-      const tileId = store.pills[ty][tx];
-      if (tileId === BASIC_PILL_ID) {
-        views.push(
-          <BasicPillView
-            key={`${tx}/${ty}`}
-            x={screenFromTileCoordinate(tx)}
-            y={screenFromTileCoordinate(ty)}
-          />
-        );
-      }
-      if (tileId === ENERGIZER_ID) {
-        views.push(
-          <EnergizerView
-            key={`${tx}/${ty}`}
-            x={screenFromTileCoordinate(tx)}
-            y={screenFromTileCoordinate(ty)}
-          />
-        );
-      }
-    }
-  }
-  return <Fragment>{views}</Fragment>;
-});
 
 const FPS: FC<{}> = observer(() => {
   const store = useStore();
