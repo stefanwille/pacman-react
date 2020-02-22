e06e652034fb484ecc590895413732ea6aca30dc
Stefan Wille <github@stefanwille.com>
2020-02-22 11:24:58 -0300

    Extract makeGhosts

--- src/lib/Game.ts
+++ src/lib/Game.ts
@@ -4,34 +4,9 @@ import { PacMan } from './PacMan';
 import { Ghost } from './Ghost';
 import { TileId, getPillsMatrix } from './MazeData';
 import { GameInterface } from './GameInterface';
+import { makeGhosts } from './makeGhosts';
 
 configure({ enforceActions: 'observed' });
-
-const makeGhosts = (game: GameInterface): Ghost[] => {
-  const ghosts: Ghost[] = [
-    new Ghost(game),
-    new Ghost(game),
-    new Ghost(game),
-    new Ghost(game),
-  ];
-
-  ghosts[0].ghostNumber = 0;
-  ghosts[0].color = 'red';
-  ghosts[0].setTileCoordinates({ x: 12, y: 11 });
-
-  ghosts[1].ghostNumber = 1;
-  ghosts[1].color = 'pink';
-  ghosts[1].setTileCoordinates({ x: 13, y: 11 });
-
-  ghosts[2].ghostNumber = 2;
-  ghosts[2].color = 'cyan';
-  ghosts[2].setTileCoordinates({ x: 14, y: 11 });
-
-  ghosts[3].ghostNumber = 3;
-  ghosts[3].color = 'orange';
-  ghosts[3].setTileCoordinates({ x: 15, y: 11 });
-  return ghosts;
-};
 
 export class Game implements GameInterface {
   constructor() {
---
+++ src/lib/makeGhosts.ts
@@ -0,0 +1,28 @@
+import { GameInterface } from './GameInterface';
+import { Ghost } from './Ghost';
+
+export const makeGhosts = (game: GameInterface): Ghost[] => {
+  const ghosts: Ghost[] = [
+    new Ghost(game),
+    new Ghost(game),
+    new Ghost(game),
+    new Ghost(game),
+  ];
+
+  ghosts[0].ghostNumber = 0;
+  ghosts[0].color = 'red';
+  ghosts[0].setTileCoordinates({ x: 12, y: 11 });
+
+  ghosts[1].ghostNumber = 1;
+  ghosts[1].color = 'pink';
+  ghosts[1].setTileCoordinates({ x: 13, y: 11 });
+
+  ghosts[2].ghostNumber = 2;
+  ghosts[2].color = 'cyan';
+  ghosts[2].setTileCoordinates({ x: 14, y: 11 });
+
+  ghosts[3].ghostNumber = 3;
+  ghosts[3].color = 'orange';
+  ghosts[3].setTileCoordinates({ x: 15, y: 11 });
+  return ghosts;
+};
