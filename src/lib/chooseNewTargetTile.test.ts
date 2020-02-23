3b6b13dc6e4c2f0e8fa2b8b29708d03bf529cd2e
Stefan Wille <github@stefanwille.com>
2020-02-23 20:40:42 -0300

    Support blinky

--- src/lib/chooseNewTargetTile.test.ts
+++ src/lib/chooseNewTargetTile.test.ts
@@ -5,13 +5,25 @@ import { chooseNextTargetTile } from './chooseNewTargetTile';
 describe('chooseNewTargetTile', () => {
   describe('chooseNewTargetTile()', () => {
     describe('in scatter mode', () => {
-      it('selects the ghosts scatter tile', () => {
+      it('returns the ghosts scatter tile', () => {
         const game = new Game();
         const ghost = game.ghosts[0];
         expect(ghost.state).toBe('scatter');
-        expect(ghost.name).toBe('Blinky');
         const tile: TileCoordinates = chooseNextTargetTile(ghost);
         expect(tile).toEqual({ x: 26, y: 1 });
+      });
+    });
+
+    describe('in chase mode', () => {
+      describe('for Blinky', () => {
+        it('returns pac mans tile', () => {
+          const game = new Game();
+          const ghost = game.ghosts[0];
+          ghost.send('PHASE_END');
+          expect(ghost.state).toBe('chase');
+          const tile: TileCoordinates = chooseNextTargetTile(ghost);
+          expect(tile).toEqual(game.pacMan.tileCoordinates);
+        });
       });
     });
   });
--- src/lib/chooseNewTargetTile.ts
+++ src/lib/chooseNewTargetTile.ts
@@ -2,5 +2,25 @@ import { TileCoordinates } from './Coordinates';
 import { Ghost } from './Ghost';
 
 export const chooseNextTargetTile = (ghost: Ghost): TileCoordinates => {
+  switch (ghost.state) {
+    case 'scatter':
+      return getTargetTileInScatterMode(ghost);
+    case 'chase':
+      return getTargetTileInChaseMode(ghost);
+    default:
+      throw new Error(`Bad state ${ghost.state}`);
+  }
+};
+
+export const getTargetTileInScatterMode = (ghost: Ghost): TileCoordinates => {
   return ghost.scatterTile;
+};
+
+export const getTargetTileInChaseMode = (ghost: Ghost): TileCoordinates => {
+  switch (ghost.ghostNumber) {
+    case 0:
+      return ghost.game.pacMan.tileCoordinates;
+    default:
+      throw new Error(`Bad name ${ghost.color}`);
+  }
 };
