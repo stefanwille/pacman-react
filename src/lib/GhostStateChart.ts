acf4f1c3c485061eceb22a61181b95ae25ec32bf
Stefan Wille <github@stefanwille.com>
2020-02-25 16:09:01 -0300

    Improve event handlers

--- src/lib/GhostStateChart.ts
+++ src/lib/GhostStateChart.ts
@@ -6,55 +6,61 @@ interface GhostEventHandler {
   onChaseToScatter(): void;
 }
 
-export const makeGhostStateChart = (eventHandler: GhostEventHandler) => {
-  const GhostStateChart = Machine({
-    id: 'ghost',
-    initial: 'scatter',
-    context: {},
-    states: {
-      chase: {
-        on: {
-          ENERGIZER_EATEN: 'frightened',
-          PHASE_END: {
-            target: 'scatter',
-            actions: context => {
-              eventHandler.onChaseToScatter();
-            },
-          },
-          COLLISION_WITH_PAC_MAN: 'scatter',
+const GhostStateChart = Machine({
+  id: 'ghost',
+  initial: 'scatter',
+  states: {
+    chase: {
+      on: {
+        ENERGIZER_EATEN: 'frightened',
+        PHASE_END: {
+          target: 'scatter',
+          actions: 'onChaseToScatter',
         },
+        COLLISION_WITH_PAC_MAN: 'scatter',
       },
-      scatter: {
-        on: {
-          ENERGIZER_EATEN: 'frightened',
-          PHASE_END: {
-            target: 'chase',
-            actions: context => {
-              eventHandler.onScatterToChase();
-            },
-          },
-          COLLISION_WITH_PAC_MAN: 'scatter',
+    },
+    scatter: {
+      on: {
+        ENERGIZER_EATEN: 'frightened',
+        PHASE_END: {
+          target: 'chase',
+          actions: 'onScatterToChase',
         },
+        COLLISION_WITH_PAC_MAN: 'scatter',
       },
-      frightened: {
-        on: {
-          ENERGIZER_TIMED_OUT: 'chase',
-          COLLISION_WITH_PAC_MAN: {
-            target: 'dead',
-            actions: context => {
-              eventHandler.onDead();
-            },
-          },
-        },
-      },
-      dead: {
-        on: {
-          REVIVED: 'chase',
+    },
+    frightened: {
+      on: {
+        ENERGIZER_TIMED_OUT: 'chase',
+        COLLISION_WITH_PAC_MAN: {
+          target: 'dead',
+          actions: 'onDead',
         },
       },
     },
-  });
+    dead: {
+      on: {
+        REVIVED: 'chase',
+      },
+    },
+  },
+});
 
-  const stateChart = interpret(GhostStateChart);
+export const makeGhostStateChart = (eventHandler: GhostEventHandler) => {
+  const extended = GhostStateChart.withConfig({
+    actions: {
+      onDead: () => {
+        eventHandler.onDead();
+      },
+      onToScatterToChase: () => {
+        eventHandler.onScatterToChase();
+      },
+      onChaseToScatter: () => {
+        eventHandler.onChaseToScatter();
+      },
+    },
+  });
+  const stateChart = interpret(extended);
   return stateChart;
 };
