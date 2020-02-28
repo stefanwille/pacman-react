a6d7c451eea59094b04eeee7041ce4f5c35c752b
Stefan Wille <github@stefanwille.com>
2020-02-28 18:32:12 +0100

    Change directions

--- src/lib/makeGhosts.ts
+++ src/lib/makeGhosts.ts
@@ -27,21 +27,25 @@ export const makeGhosts = (game: Game): Ghost[] => {
   ghosts[0].name = 'Blinky';
   ghosts[0].color = 'red';
   ghosts[0].colorCode = '#ff0000';
+  ghosts[0].direction = 'LEFT';
 
   ghosts[1].ghostNumber = 1;
   ghosts[1].name = 'Pinky';
   ghosts[1].color = 'pink';
   ghosts[1].colorCode = '#fcb5ff';
+  ghosts[1].direction = 'RIGHT';
 
   ghosts[2].ghostNumber = 2;
   ghosts[2].name = 'Inky';
   ghosts[2].color = 'blue';
   ghosts[2].colorCode = '#00ffff';
+  ghosts[3].direction = 'LEFT';
 
   ghosts[3].ghostNumber = 3;
   ghosts[3].name = 'Clyde';
   ghosts[3].color = 'orange';
   ghosts[3].colorCode = '#f9ba55';
+  ghosts[3].direction = 'RIGHT';
 
   resetGhosts(ghosts);
   return ghosts;
