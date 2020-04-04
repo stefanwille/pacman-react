d8b0ab6417ca726e71f732bfd11f99dc4b79892b
Stefan Wille <github@stefanwille.com>
2020-04-04 11:06:06 +0200

    Global styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import 'normalize.css';
 import 'antd/dist/antd.css';
 
-import './App.css';
+import './GlobalStyles.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes } from './Routes';
 import { AppMenu } from './components/AppMenu';
