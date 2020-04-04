8392d81ea8db315eb337c888288d3131d97d7f93
Stefan Wille <github@stefanwille.com>
2020-04-04 11:34:39 +0200

    Layout

--- src/components/GhostsDebugView.tsx
+++ src/components/GhostsDebugView.tsx
@@ -1,24 +1,21 @@
 import { Card } from 'antd';
 import { observer } from 'mobx-react-lite';
 import React, { FC } from 'react';
-import styled from 'styled-components/macro';
 import { Ghost } from '../lib/Ghost';
 import { GhostDebugControls } from './GhostDebugControls';
 import { GhostsDebugTable } from './GhostDebugTable';
 import { Spacer } from './Spacer';
 
-const Layout = styled.div``;
-
 type RenderGhost = (ghost: Ghost) => JSX.Element | string;
 
 export const GhostsDebugView: FC<{}> = observer(() => {
   return (
-    <Layout className="GhostsDebugView">
+    <div className="GhostsDebugView">
       <Card title="Ghosts" size="small">
         <GhostsDebugTable />
         <Spacer />
         <GhostDebugControls />
       </Card>
-    </Layout>
+    </div>
   );
 });
