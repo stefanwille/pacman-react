24a781c969e42c00f27da413632bfefea47c589a
Stefan Wille <github@stefanwille.com>
2020-04-13 17:35:15 +0200

    Layout

--- src/components/GhostDebugControls.tsx
+++ src/components/GhostDebugControls.tsx
@@ -11,49 +11,45 @@ export const GhostDebugControls: FC<{}> = observer(() => {
   const store = useStore();
   return (
     <div className="GhostDebugControls">
-      <Space direction="vertical" style={{ width: '100%' }}>
-        <Row>
-          <Col flex="60px">
-            <Switch
-              checked={store.debugState.ghostViewOptions.target}
-              onChange={action(checked => {
-                store.debugState.ghostViewOptions.target = checked;
-              })}
-            />
-          </Col>
-          <Col flex="rest">
-            <Text>Show Target Tile</Text>
-          </Col>
-        </Row>
+      <Row style={{ width: 'auto' }}>
+        <Col flex="0 0 56px">
+          <Switch
+            checked={store.debugState.ghostViewOptions.target}
+            onChange={action(checked => {
+              store.debugState.ghostViewOptions.target = checked;
+            })}
+          />
+        </Col>
+        <Col flex="0 0 auto">
+          <Text>Show Target Tile</Text>
+        </Col>
+        <Col flex="0 0 48px"></Col>
 
-        <Row>
-          <Col flex="60px">
-            <Switch
-              checked={store.debugState.ghostViewOptions.wayPoints}
-              onChange={action(checked => {
-                store.debugState.ghostViewOptions.wayPoints = checked;
-              })}
-            />
-          </Col>
-          <Col flex="rest">
-            <Text>Show Waypoints</Text>
-          </Col>
-        </Row>
+        <Col flex="0 0 56px">
+          <Switch
+            checked={store.debugState.ghostViewOptions.wayPoints}
+            onChange={action(checked => {
+              store.debugState.ghostViewOptions.wayPoints = checked;
+            })}
+          />
+        </Col>
+        <Col flex="0 0 auto">
+          <Text>Show Waypoints</Text>
+        </Col>
+        <Col flex="0 0 48px"></Col>
 
-        <Row>
-          <Col flex="60px">
-            <Switch
-              checked={store.debugState.ghostViewOptions.hitBox}
-              onChange={action(checked => {
-                store.debugState.ghostViewOptions.hitBox = checked;
-              })}
-            />
-          </Col>
-          <Col flex="rest">
-            <Text>Show Hitbox</Text>
-          </Col>
-        </Row>
-      </Space>
+        <Col flex="0 0 56px">
+          <Switch
+            checked={store.debugState.ghostViewOptions.hitBox}
+            onChange={action(checked => {
+              store.debugState.ghostViewOptions.hitBox = checked;
+            })}
+          />
+        </Col>
+        <Col flex="0 0 auto">
+          <Text>Show Hitbox</Text>
+        </Col>
+      </Row>
     </div>
   );
 });
