d34ecf18b0f7dd84502492d47de432d8afb7e4de
Stefan Wille <github@stefanwille.com>
2020-04-14 18:41:45 +0200

    Margin

--- src/pages/MazeTestPage/MazeTestPage.tsx
+++ src/pages/MazeTestPage/MazeTestPage.tsx
@@ -7,34 +7,56 @@ import {
   screenFromTileCoordinate,
   SCREEN_TILE_CENTER,
 } from '../../lib/Coordinates';
+import styled from 'styled-components/macro';
+import { Row, Col } from 'antd';
 
 export const MazeTestPage: React.FC = () => {
   return (
-    <div
-      style={{
-        position: 'relative',
-      }}
-    >
-      <Sprite className="Sprite-maze" name="maze-state-empty" x={0} y={0} />
-      <GridWithHoverCoordinates screenCoordinates={{ x: 0, y: 0 }} />
-      <PillsView />
-      <Sprite
-        className="Sprite-maze"
-        name="maze-state-empty"
-        x={16 * 16 * 3}
-        y={0}
-      />
-      <GridWithHoverCoordinates screenCoordinates={{ x: 16 * 16 * 3, y: 0 }} />
-      <Sprite x={8 * 3 * 1} y={8 * 3 * 1} name="basic-pill"></Sprite>
-      <Sprite x={8 * 3 * 1} y={8 * 3 * 3} name="energizer"></Sprite>
-      <Sprite x={8 * 3 * 6} y={8 * 3 * 10} name="energizer"></Sprite>
-      <PacManSprite
-        direction="RIGHT"
-        phase={1}
-        x={screenFromTileCoordinate(1) + SCREEN_TILE_CENTER}
-        y={screenFromTileCoordinate(3) + SCREEN_TILE_CENTER}
-      />
-      <BasicPillHitBox />
-    </div>
+    <Layout>
+      <Row justify="space-around">
+        <Col flex="562px">
+          <RelativeAbsoluteLayout>
+            <Sprite
+              className="Sprite-maze"
+              name="maze-state-empty"
+              x={0}
+              y={0}
+            />
+            <GridWithHoverCoordinates screenCoordinates={{ x: 0, y: 0 }} />
+            <PillsView />
+          </RelativeAbsoluteLayout>
+        </Col>
+
+        <Col flex="562px">
+          <RelativeAbsoluteLayout>
+            <Sprite
+              className="Sprite-maze"
+              name="maze-state-empty"
+              x={0}
+              y={0}
+            />
+            <GridWithHoverCoordinates screenCoordinates={{ x: 0, y: 0 }} />
+            <Sprite x={20 * 1} y={20 * 1} name="basic-pill"></Sprite>
+            <Sprite x={20 * 1} y={20 * 3} name="energizer"></Sprite>
+            <Sprite x={20 * 6} y={20 * 10} name="energizer"></Sprite>
+            <PacManSprite
+              direction="RIGHT"
+              phase={1}
+              x={screenFromTileCoordinate(1) + SCREEN_TILE_CENTER}
+              y={screenFromTileCoordinate(4) + SCREEN_TILE_CENTER}
+            />
+            <BasicPillHitBox />
+          </RelativeAbsoluteLayout>
+        </Col>
+      </Row>
+    </Layout>
   );
 };
+
+const Layout = styled.div`
+  margin: 32px;
+`;
+
+const RelativeAbsoluteLayout = styled.div`
+  position: relative;
+`;
