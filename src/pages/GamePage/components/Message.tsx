a36bdddb0532be71bb4f6b56d6d03323e26b1279
Stefan Wille <github@stefanwille.com>
2020-04-14 19:39:33 +0200

    Improve

--- src/pages/GamePage/components/Message.tsx
+++ src/pages/GamePage/components/Message.tsx
@@ -1,11 +1,20 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import { observer } from 'mobx-react-lite';
 import React, { FC } from 'react';
-import './Message.css';
-import classNames from 'classnames';
+import styled from 'styled-components/macro';
 
 export const Message: FC<{ className?: string; text: string }> = observer(
   ({ className, text }) => {
-    return <span className={classNames('Message', className)}>{text}</span>;
+    return <MessageStyled className={className}>{text}</MessageStyled>;
   }
 );
+
+const MessageStyled = styled.span`
+  font-family: Joystix;
+  font-size: 24px;
+  color: yellow;
+  position: absolute;
+  left: 170px;
+  top: 332px;
+  width: 220px;
+  text-align: center;
+`;
