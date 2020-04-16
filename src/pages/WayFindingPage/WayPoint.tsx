d424a60eced2e7e37b8b089cea5080a7bc7992cc
Stefan Wille <github@stefanwille.com>
2020-04-16 19:39:18 +0200

    Click through waypoints

--- src/pages/WayFindingPage/WayPoint.tsx
+++ src/pages/WayFindingPage/WayPoint.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import './WayPoint.css';
 import { ScreenCoordinates, SCREEN_TILE_CENTER } from '../../model/Coordinates';
+import styled from 'styled-components/macro';
 
 export const WayPoint: React.FC<{
   screenCoordinates: ScreenCoordinates;
   style?: any;
 }> = ({ screenCoordinates, style }) => (
-  <div
-    className="WayPoint"
+  <StyledWaypoint
     style={{
       ...style,
       left: `${screenCoordinates.x + SCREEN_TILE_CENTER - 8}px`,
       top: `${screenCoordinates.y + SCREEN_TILE_CENTER - 8}px`,
     }}
-  ></div>
+  />
 );
+
+const StyledWaypoint = styled.div`
+  position: relative;
+  width: calc(var(--SCREEN_TILE_SIZE) / 2);
+  height: calc(var(--SCREEN_TILE_SIZE) / 2);
+  background-color: #e2a12b;
+  position: absolute;
+  transform-origin: top left;
+  margin: 3.5px;
+  border-radius: 4px;
+
+  pointer-events: none;
+`;
