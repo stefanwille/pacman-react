e534d10da229e7d7af3948066f55fdb425e87ca6
Stefan Wille <github@stefanwille.com>
2020-04-26 20:00:03 +0200

    Waypoints with direction

--- src/pages/WayFindingPage/WayPoint.tsx
+++ src/pages/WayFindingPage/WayPoint.tsx
@@ -1,27 +1,49 @@
 import React from 'react';
-import { ScreenCoordinates, SCREEN_TILE_CENTER } from '../../model/Coordinates';
+import { ScreenCoordinates, SCREEN_TILE_SIZE } from '../../model/Coordinates';
 import styled from 'styled-components/macro';
+import { Direction } from '../../model/Types';
 
-const DOT_SIZE = 8;
-const CENTER_OFFSET = SCREEN_TILE_CENTER - DOT_SIZE / 2;
+const x1 = 30;
+const y1 = 30;
+const x2 = 70;
+const y2 = 70;
+const x3 = 50;
+const POINTS = `${x1},${y1} ${x2},${y1} ${x3},${y2} ${x1},${y1}`;
+
+const DirectionToAngle = {
+  DOWN: 0,
+  LEFT: 90,
+  UP: 180,
+  RIGHT: 270,
+};
 
 export const WayPoint: React.FC<{
   screenCoordinates: ScreenCoordinates;
   color: string;
-}> = ({ screenCoordinates, color }) => (
-  <StyledWaypoint
-    style={{
-      backgroundColor: color,
-      left: `${screenCoordinates.x + CENTER_OFFSET}px`,
-      top: `${screenCoordinates.y + CENTER_OFFSET}px`,
-    }}
-  />
-);
+  direction: Direction;
+}> = ({ screenCoordinates, color, direction }) => {
+  const angle = DirectionToAngle[direction];
+  return (
+    <SvgStyled
+      viewBox="0 0 100 100"
+      height="6"
+      width="6"
+      style={{
+        left: `${screenCoordinates.x + 1}px`,
+        top: `${screenCoordinates.y}px`,
+      }}
+    >
+      <g transform={`rotate(${angle} 50 50)`}>
+        <polygon points={POINTS} fill={color} stroke={color} strokeWidth={1} />
+      </g>
+    </SvgStyled>
+  );
+};
 
-const StyledWaypoint = styled.div`
+const SvgStyled = styled.svg`
   position: absolute;
-  width: ${DOT_SIZE}px;
-  height: ${DOT_SIZE}px;
-  border-radius: ${DOT_SIZE / 2}px;
+  display: block;
+  width: ${SCREEN_TILE_SIZE}px;
+  height: ${SCREEN_TILE_SIZE}px;
   pointer-events: none;
 `;
--- src/pages/WayFindingPage/WayPoints.tsx
+++ src/pages/WayFindingPage/WayPoints.tsx
@@ -3,6 +3,9 @@ import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { screenFromTile, TileCoordinates } from '../../model/Coordinates';
 import { WayPoint } from './WayPoint';
+import { getDirectionFromTileToTile } from '../../model/getDirectionFromTileToTile';
+import { Direction } from '../../model/Types';
+import { assert } from '../../util/assert';
 
 export const WayPoints: React.FC<{
   wayPoints: TileCoordinates[];
@@ -11,13 +14,30 @@ export const WayPoints: React.FC<{
   <>
     {wayPoints.map((wayPoint, index) => {
       const screenCoordinates = screenFromTile(wayPoint);
+      const direction = getDirection(wayPoints, index);
       return (
         <WayPoint
           key={index}
           screenCoordinates={screenCoordinates}
           color={color}
+          direction={direction}
         />
       );
     })}
   </>
 ));
+
+const getDirection = (
+  wayPoints: TileCoordinates[],
+  index: number
+): Direction => {
+  if (wayPoints.length <= 1) {
+    return 'DOWN';
+  }
+  const indexToUse = index + 1 < wayPoints.length ? index : index - 1;
+  const fromTile = wayPoints[indexToUse];
+  const toTile = wayPoints[indexToUse + 1];
+  assert(toTile, `${indexToUse} ${wayPoints.length}`);
+  const direction = getDirectionFromTileToTile(fromTile, toTile);
+  return direction;
+};
