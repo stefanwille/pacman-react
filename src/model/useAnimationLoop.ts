de781e84bf68601ec07bae6bf360c35523f9d622
Stefan Wille <github.stefanwille@gmail.com>
2026-01-22 09:09:52 +0100

    Ensure consistent frame rate including macbook pro

--- src/model/useAnimationLoop.ts
+++ src/model/useAnimationLoop.ts
@@ -1,13 +1,32 @@
 import { useEffect, useRef } from 'react';
 import { MilliSeconds } from './Types';
 
-type AnimationStepFunc = (timestamp: MilliSeconds) => void;
+type AnimationStepFunc = (currentTime: MilliSeconds) => void;
+
+// 60 animation steps per second
+const TICK_DURATION = 1000 / 60;
+
 
 export const useAnimationLoop = (animationStep: AnimationStepFunc) => {
+  // The requestAnimationFrame call id
   const requestRef = useRef(-1);
+  // The last time the animation step was called
+  const lastTimeRef = useRef(performance.now());
+  // The accumulated time since the last update
+  const accumulatorRef = useRef(0);
 
-  const animate = (timestamp: number) => {
-    animationStep(timestamp);
+  const animate = (currentTime: number) => {
+    accumulatorRef.current += currentTime - lastTimeRef.current;
+    lastTimeRef.current = currentTime;
+
+    // Ensure a consistent, fixed number of animation steps per second:
+    //   If we get less than 60 animation frames per second, the while loop may perform multiple animation steps in a single frame.
+    //   If we get more than 60 animation frames per second, the while loop may skip some updates. This happens with Apple's 120Hz ProMotion display, such as the MacBook Pro Apple Silicon.
+    while (accumulatorRef.current >= TICK_DURATION) {
+      animationStep(currentTime);
+      accumulatorRef.current -= TICK_DURATION;
+    }
+
     requestRef.current = requestAnimationFrame(animate);
   };
 
