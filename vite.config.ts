35e8f1b159760e18c79b1db0a43394c1a93e16bb
Stefan Wille <github.stefanwille@gmail.com>
2026-04-11 18:54:28 +0200

    Update Vite configuration to set chunk size warning limit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000, open: true },
-  build: { outDir: 'build' },
+  build: { outDir: 'build', chunkSizeWarningLimit: 1000 },
   test: {
     globals: true,
     environment: 'jsdom',
